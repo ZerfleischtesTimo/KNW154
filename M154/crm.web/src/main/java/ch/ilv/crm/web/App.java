5b0d2a9e1fa3a8e5d4e6f58f3ef8a00a6b254cbb
timo <timo.schaub@ict-scouts.ch>
2022-07-01 13:58:15 +0200

    initial project

---
+++ M154/crm.web/src/main/java/ch/ilv/crm/web/App.java
@@ -0,0 +1,13 @@
+package ch.ilv.crm.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
